c49fc7930ae5d7cd0b2ca9d67f3476a4efc319b5
piotrek <piotrekgtpll@gmail.com>
2023-08-25 08:40:28 +0200

    TZluamczenia

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/')]
+class IndexController extends AbstractController
+{
+    #[Route(
+        name: 'empty_index',
+        methods: 'GET'
+    )]
+    public function index(): RedirectResponse
+    {
+        return $this->redirectToRoute('note_index');
+    }
+}
